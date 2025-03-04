97cc3019010d8c97a61b0d4881d80a2fabe16d15
Muhammad Suleman <msulemvn@gmail.com>
2025-03-04 19:06:55 +0500

    Use Vee Validate form component

--- resources/js/Components/MyDialog.vue
+++ resources/js/Components/MyDialog.vue
@@ -9,90 +9,123 @@ import {
     DialogTitle
 } from "@/Components/ui/dialog";
 import { Input } from "@/Components/ui/input";
-import { Label } from "@/Components/ui/label";
-import { ref, onMounted, watch } from "vue";
+import { ref, watch, computed } from "vue";
 import axios from "axios";
+import { useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+import * as z from 'zod';
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/Components/ui/form';
+import { User } from "lucide-vue-next";
+
+const formSchema = toTypedSchema(z.object({
+    name: z.string().min(2).max(50),
+    email: z.string().email(),
+    password: z.string().min(6).optional(),
+    password_confirmation: z.string().min(6).optional(),
+}));
 
 const props = defineProps({
-    user: {
-        type: Object,
-        default: {},
-        required: false,
-    },
-    operation: {
-        type: String,
-    },
-    isDialogOpen: {
-        type: Boolean,
-    },
+    user: Object,
+    operation: String,
+    isDialogOpen: Boolean,
 });
 
 const emit = defineEmits(["onCreate", "onEdit", "onDialogClose"]);
 
 const user = ref({ ...props.user });
-const isDialogOpen = ref(props.isDialogOpen);
 
-onMounted(() => { });
+const form = useForm({
+    validationSchema: formSchema,
+    initialValues: props.user || {},
+});
 
-watch(isDialogOpen, (current, previous) => {
-    if (current != previous) {
-        emit("onDialogClose", current);
-    }
+const isDialogOpen = computed({
+    get: () => props.isDialogOpen,
+    set: (value) => emit("onDialogClose", value),
 });
 
 watch(() => props.user, (newUser) => {
-    user.value = { ...newUser };
+    form.setValues(newUser || {});
 });
 
-const onSubmitForm = () => {
-    axios({
-        method: props.operation == "Create" ? "post" : "put",
-        url: `/users${props.operation == "Create" ? "" : "/" + user.value.id}`,
-        data: user.value,
-        responseType: "json",
-    }).then(function (response) {
+const onSubmit = form.handleSubmit(async (values) => {
+    try {
+        const response = await axios({
+            method: props.operation === "Create" ? "post" : "put",
+            data: values,
+            url: `/users${props.operation === "Create" ? "" : "/" + user.value.id}`,
+            responseType: "json",
+        });
+
         if (response.status === 201) {
-            emit("onCreate", response.data.data)
+            emit("onCreate", response.data.data);
         } else {
-            emit("onEdit", response.data.data)
+            emit("onEdit", response.data.data);
         }
         isDialogOpen.value = false;
-    });
-};
+        form.resetForm();
+    } catch (error) {
+        console.error("Error submitting form:", error);
+    }
+});
 </script>
 
 <template>
     <Dialog v-model:open="isDialogOpen">
         <DialogContent class="sm:max-w-[425px]">
-            <form @submit.prevent="onSubmitForm">
+            <form @submit.prevent="onSubmit">
                 <DialogHeader>
                     <DialogTitle>{{ props.operation }}</DialogTitle>
                     <DialogDescription>
-                        Make changes to your profile here. Click save when
-                        you're done.
+                        Make changes to your profile here. Click save when you're done.
                     </DialogDescription>
                 </DialogHeader>
                 <div class="grid gap-4 py-4">
-                    <div class="grid grid-cols-4 items-center gap-4">
-                        <Label for="name" class="text-right"> Name </Label>
-                        <Input id="name" name="name" :default-value="user.name" v-model:model-value="user.name"
-                            class="col-span-3" />
-                    </div>
-                    <div class="grid grid-cols-4 items-center gap-4">
-                        <Label for="email" class="text-right"> Email </Label>
-                        <Input id="email" name="email" :default-value="user.email" v-model:model-value="user.email"
-                            class="col-span-3" />
-                    </div>
-                    <div class="grid grid-cols-4 items-center gap-4" v-if="props.operation == 'Create'">
-                        <Label for="password" class="text-right"> Password </Label>
-                        <Input id="password" type="password" name="password" v-model:model-value="user.password"
-                            class="col-span-3" />
-                    </div>
-                    <div class="grid grid-cols-4 items-center gap-4" v-if="props.operation == 'Create'">
-                        <Label for="password_confirmation" class="text-right"> Confirm Password </Label>
-                        <Input id="password_confirmation" type="password" name="password_confirmation"
-                            v-model:model-value="user.password_confirmation" class="col-span-3" />
-                    </div>
+                    <FormField v-slot="{ componentField }" name="name">
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input type="text" placeholder="Enter your name" v-bind="componentField" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    </FormField>
+
+                    <FormField v-slot="{ componentField }" name="email">
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input type="email" placeholder="Enter your email" v-bind="componentField" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    </FormField>
+
+                    <FormField v-if="operation === 'Create'" v-slot="{ componentField }" name="password">
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input type="password" placeholder="Enter your password" v-bind="componentField" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    </FormField>
+
+                    <FormField v-if="operation === 'Create'" v-slot="{ componentField }" name="password_confirmation">
+                        <FormItem>
+                            <FormLabel>Confirm Password</FormLabel>
+                            <FormControl>
+                                <Input type="password" placeholder="Confirm your password" v-bind="componentField" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    </FormField>
                 </div>
                 <DialogFooter>
                     <Button type="submit"> Save changes </Button>
